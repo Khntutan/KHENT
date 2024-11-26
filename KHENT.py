c849fccf4f5825ec3d4e48a96e801df3c789f02b
Khntutan <paqueroskhent06@gmail.com>
2024-11-26 17:14:20 +0800

    Update KHENT.py

--- KHENT.py
+++ KHENT.py
@@ -19,7 +19,7 @@ def create_biography():
     
     # Collect hobbies and family details
     st.header("Hobbies and Family")
-    hobbies = st.text_area("my hobbies is watching anime")
+    hobbies = st.text_area("Hobbies")
     
     # Image upload for profile picture
     st.header("Profile Picture (Optional)")
